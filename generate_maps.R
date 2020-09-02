b3f59ecb3eafb1e362b61dcc767a5f849c1a749e
Francisco E. Fontúrbel <fonturbel@gmail.com>
2020-09-02 12:38:26 -0400

    New script

---
+++ generate_maps.R
@@ -0,0 +1,9 @@
+##This short script will help you getting hands-on with the database
+##and plot locations in a map
+
+#Load the database
+data<-read.csv("ColombiaBirdMorphologyPhenology.csv", header = T)
+attach(data)
+
+head(data)
+
